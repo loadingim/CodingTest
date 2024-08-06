70d0c38e3c52bf49b392cba62d44a3e4b8313e86
loadingim <68083530+loadingim@users.noreply.github.com>
2024-08-06 12:18:57 +0900

    Update 2_1.cs

--- Day_2/2_1.cs
+++ Day_2/2_1.cs
@@ -3,6 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 namespace Day_2
 {
+    // <summary
+    // 2024년 8월 13일
+    // 문제 : 문제 설명
+    // 주어진 숫자 중 3개의 수를 더했을 때 소수가 되는 경우의 개수를 구하려고 합니다.숫자들이 들어있는 배열 nums가 매개변수로 주어질 때,
+    // nums에 있는 숫자들 중 서로 다른 3개를 골라 더했을 때 소수가 되는 경우의 개수를 return 하도록 solution 함수를 완성해주세요.
+    // 제한사항
+    // nums에 들어있는 숫자의 개수는 3개 이상 50개 이하입니다.
+    // nums의 각 원소는 1 이상 1,000 이하의 자연수이며, 중복된 숫자가 들어있지 않습니다.
+    // URL : https://school.programmers.co.kr/learn/courses/30/lessons/12977
+    // </summary>
     internal class Program
     {
 
@@ -57,7 +67,7 @@ namespace Day_2
         static void Main(string[] args)
         {
             Solution solo = new Solution();
-            int[] number = new int[5] { 1, 2, 7, 6, 4};
+            int[] number = new int[5] { 1, 2, 7, 6, 4 };
             int count = solo.solution(number);
             Console.WriteLine(count);
         }
