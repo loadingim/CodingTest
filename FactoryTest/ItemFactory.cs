d2337974bdd8ef02e81012e394a48dabe6ec12dc
loadingim <68083530+loadingim@users.noreply.github.com>
2024-08-11 01:20:53 +0900

    끝
    끝

---
+++ FactoryTest/ItemFactory.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum ItemType
+{
+    Potion, Weapon, Armor, Food
+}
+
+public class Item
+{
+    public string name;
+    public ItemType type;
+    public int price;
+
+    public void print(int i)
+    {
+        Console.WriteLine($"{i+1}번 아이템");
+        Console.WriteLine($"아이템 타입 : {type}\n아이템 이름 : {name}\n아이템 가격 : {price} ");
+        Console.WriteLine();
+    }
+}
+
+public class Potion : Item
+{
+    public Potion()
+    {
+        type = ItemType.Potion;
+        name = "포션";
+        price = 50;
+    }
+}
+public class Weapon : Item
+{
+    public Weapon()
+    {
+        type = ItemType.Weapon;
+        name = "무기";
+        price = 800;
+    }
+}
+public class Armor : Item
+{
+    public Armor()
+    {
+        type = ItemType.Armor;
+        name = "갑옷";
+        price = 500;
+    }
+}
+public class Food : Item
+{
+    public Food()
+    {
+        type = ItemType.Food;
+        name = "음식";
+        price = 25;
+    }
+}
+
+public class ItemFactory
+{
+    public static Item Create(ItemType type)
+    {
+        switch (type)
+        {
+            case ItemType.Potion:
+                Potion potion = new Potion();
+                return potion;
+            case ItemType.Weapon:
+                Weapon weapon = new Weapon();
+                return weapon;
+            case ItemType.Armor:
+                Armor armor = new Armor();
+                return armor;
+            case ItemType.Food:
+                Food food = new Food();
+                return food;
+            default:
+                return null;
+        }
+    }
+}
--- FactoryTest/Program.cs
+++ FactoryTest/Program.cs
@@ -6,89 +6,10 @@ namespace FactoryTest
 {
     internal class Program
     {
-        public enum ItemType
-        {
-            Potion, Weapon, Armor, Food
-        }
-
-        public class Item
-        {
-            public string name;
-            public ItemType type;
-            public int price;
-
-            public void print()
-            {
-                Console.WriteLine($"아이템 타입 : {type}\n아이템 이름 : {name}\n아이템 가격 : {price} ");
-            }
-        }
-
-        public class Potion : Item
-        {
-            public Potion()
-            {
-                type = ItemType.Potion;
-                name = "포션";
-                price = 50;
-            }
-        }
-        public class Weapon : Item
-        {
-            public Weapon()
-            {
-                type = ItemType.Weapon;
-                name = "무기";
-                price = 800;
-            }
-        }
-        public class Armor : Item
-        {
-            public Armor()
-            {
-                type = ItemType.Armor;
-                name = "갑옷";
-                price = 500;
-            }
-        }
-        public class Food : Item
-        {
-            public Food()
-            {
-                type = ItemType.Food;
-                name = "음식";
-                price = 25;
-            }
-        }
-
-        public class ItemFactory
-        {
-            public static Item Create(ItemType type)
-            {
-                switch (type)
-                {
-                    case ItemType.Potion:
-                        Potion potion = new Potion();
-                        return potion;
-                    case ItemType.Weapon:
-                        Weapon weapon = new Weapon();
-                        return weapon;
-                    case ItemType.Armor:
-                        Armor armor = new Armor();
-                        return armor;
-                    case ItemType.Food:
-                        Food food = new Food();
-                        return food;
-                    default:
-                        return null;
-                }
-            }
-        }
-
-
         static void Main(string[] args)
         {
 
-            // 자꾸 객체 생성이 안되서 우수 코드 참고했습니다 as넣으니까 되더라구요 제가 아직 잘 이해를 못해서 왜 그런지 피드백 한번 부탁드립니다...
+            // 자꾸 객체 생성이 안되서 우수 코드 참고했습니다 as넣으니까 되더라구요 제가 아직 잘 이해를 못해서 왜 그런지 피드백으로 설명 해주실 수 있으신가요...
             Potion potion = ItemFactory.Create(ItemType.Potion) as Potion;
             Weapon weapon = ItemFactory.Create(ItemType.Weapon) as Weapon;
             Armor armor = ItemFactory.Create(ItemType.Armor) as Armor;
@@ -98,7 +19,7 @@ namespace FactoryTest
             
             for (int i = 0; i < items.Length; i++)
             {
-                items[i].print();
+                items[i].print(i);
             }
         }
     }
